03796f57ba782ff57bacf51df1b3bbbe30788f41
Tsiry Valisoa <rotsiniaina.tsiry@gmail.com>
2025-12-17 13:40:48 +0300

    fix: inventory name in main

--- src/main/java/mg/tsiry/invetory_management_system/InventoryManagementSystemApplication.java
+++ src/main/java/mg/tsiry/invetory_management_system/InventoryManagementSystemApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class InvetoryManagementSystemApplication {
+public class InventoryManagementSystemApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(InvetoryManagementSystemApplication.class, args);
+		SpringApplication.run(InventoryManagementSystemApplication.class, args);
 	}
 
 }
